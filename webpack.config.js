0ec59c0fe415bd61faadeb5486b330cb80e9fecf
José Muñoz R <jozeepp@gmail.com>
2017-01-16 23:11:18 -0300

    Base - add webpack.config.js - add src/app.jsx

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports ={
+  resolve: {
+    extensions: ['', '.js','.jsx']
+  },
+  context: __dirname,
+  entry: {
+    app: ['./src/app.jsx']
+  },
+  output: {
+    path: './build',
+    filename: 'app.js',
+    publicPath: '/build/'
+  },
+  devServer:{
+    host: '0.0.0.0',
+    port: 8080,
+    inline: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx?)$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react']
+        }
+      }
+    ]
+  }
+}
